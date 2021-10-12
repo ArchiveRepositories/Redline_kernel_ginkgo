d49ff5aec2a72f8dfd9d82a03ffdccb6086c56a2
johnmart19 <johnivan19999@gmail.com>
2021-10-13 00:42:10 +0300

    Ginkgo: Redline kernel Init & Regen defconfig

--- build.sh
+++ build.sh
@@ -4,63 +4,57 @@
 # Copyright (C) 2020-2021 Adithya R.
 
 SECONDS=0 # builtin bash timer
-ZIPNAME="QuicksilveRV2-ginkgo-$(date '+%Y%m%d-%H%M').zip"
-TC_DIR="$HOME/tc/azure-clang"
-AK3_DIR="$HOME/android/AnyKernel3"
+ZIPNAME="Redline-ginkgo-$(date '+%Y%m%d-%H%M').zip"
+TC_DIR="$HOME/proton-clang-build/install"
 DEFCONFIG="vendor/ginkgo-perf_defconfig"
 
 export PATH="$TC_DIR/bin:$PATH"
 
 if ! [ -d "$TC_DIR" ]; then
-echo "Azure clang not found! Cloning to $TC_DIR..."
-if ! git clone -q -b main --depth=1 https://gitlab.com/Panchajanya1999/azure-clang $TC_DIR; then
+echo "Proton clang not found! Cloning to $TC_DIR..."
+if ! git clone -q --depth=1 --single-branch https://github.com/kdrag0n/proton-clang $TC_DIR; then
 echo "Cloning failed! Aborting..."
 exit 1
 fi
 fi
 
-export KBUILD_BUILD_USER=adithya
-export KBUILD_BUILD_HOST=ghostrider_reborn
-
-if [[ $1 = "-r" || $1 = "--regen" ]]; then
-make O=out ARCH=arm64 $DEFCONFIG savedefconfig
-cp out/defconfig arch/arm64/configs/$DEFCONFIG
-exit
-fi
-
-if [[ $1 = "-c" || $1 = "--clean" ]]; then
-rm -rf out
-fi
+export KBUILD_BUILD_USER=johnmart19
+export KBUILD_BUILD_HOST=Johnmart19-PC
 
 mkdir -p out
 make O=out ARCH=arm64 $DEFCONFIG
 
+if [[ $1 == "-r" || $1 == "--regen" ]]; then
+cp out/.config arch/arm64/configs/$DEFCONFIG
+echo -e "\nRegened defconfig succesfully!"
+exit 0
+else
 echo -e "\nStarting compilation...\n"
-make -j$(nproc --all) O=out ARCH=arm64 CC=clang LD=ld.lld AR=llvm-ar AS=llvm-as NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi- Image.gz-dtb dtbo.img
+make -j$(nproc --all) O=out ARCH=arm64 CC="ccache clang" LD=ld.lld AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi- Image.gz-dtb dtbo.img
+fi
 
 if [ -f "out/arch/arm64/boot/Image.gz-dtb" ] && [ -f "out/arch/arm64/boot/dtbo.img" ]; then
 echo -e "\nKernel compiled succesfully! Zipping up...\n"
-if [ -d "$AK3_DIR" ]; then
-cp -r $AK3_DIR AnyKernel3
-elif ! git clone -q https://github.com/ghostrider-reborn/AnyKernel3; then
-echo -e "\nAnyKernel3 repo not found locally and cloning failed! Aborting..."
+if ! [ -d "AnyKernel3" ]; then
+echo "AnyKernel3 not found! Cloning..."
+if ! git clone -q --depth=1 --single-branch https://github.com/johnmart19/AnyKernel3 -b redline_ginkgo AnyKernel3; then
+echo "Cloning failed! Aborting..."
 exit 1
+fi
 fi
 cp out/arch/arm64/boot/Image.gz-dtb AnyKernel3
 cp out/arch/arm64/boot/dtbo.img AnyKernel3
 rm -f *zip
 cd AnyKernel3
-git checkout master &> /dev/null
 zip -r9 "../$ZIPNAME" * -x '*.git*' README.md *placeholder
 cd ..
-rm -rf AnyKernel3
-rm -rf out/arch/arm64/boot
 echo -e "\nCompleted in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s) !"
+if command -v gdrive &> /dev/null; then
+gdrive upload --share $ZIPNAME
+else
 echo "Zip: $ZIPNAME"
-if ! [[ $HOSTNAME = "RyzenBeast" && $USER = "adithya" ]]; then
-curl --upload-file $ZIPNAME http://transfer.sh/$ZIPNAME; echo
 fi
+rm -rf out/arch/arm64/boot
 else
 echo -e "\nCompilation failed!"
-exit 1
 fi
